792be8d4afd8b821425a0bec40503cd45dd13906
dofla <ismael.saindou@gmail.com>
2016-09-16 17:02:52 +0200

    update view form contact

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Isma
+ * Date: 16/09/2016
+ * Time: 15:24
+ */
+
+namespace AppBundle\Controller;
+
+
+class ContactController
+{
+
+
+    public function formAction()
+    {
+
+    }
+}
--- src/AppBundle/Form/ContactType.php
+++ src/AppBundle/Form/ContactType.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,9 +18,10 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('content')
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('content', TextareaType::class)
         ;
     }
     
