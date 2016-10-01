8347fcf3ea642f6c6c34d11ff2739134bffd9e44
dofla <ismael.saindou@gmail.com>
2016-10-02 01:04:50 +0200

    add contact form type in her view & config submit in ajax

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -9,12 +9,44 @@
 namespace AppBundle\Controller;
 
 
-class ContactController
+use AppBundle\Entity\Contact;
+use AppBundle\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends Controller
 {
 
-
-    public function formAction()
+    public function formAction(Request $request)
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
 
+        if ($request->isXmlHttpRequest()) {
+            if ($form->handleRequest($request)->isValid() && $form->isSubmitted()) {
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($contact);
+                $em->flush();
+
+                $contactModal = $this->render(':components:contact-modal.html.twig',
+                    [
+                        'form'  => $form->createView(),
+                    ]
+                )->getContent();
+
+                return new JsonResponse(array(
+                    'data' => 'donné enregistré',
+                    'contactModal' => $contactModal
+                ));
+            }
+        }
+
+        return $this->render(':components:contact-modal.html.twig',
+            [
+                'form'  => $form->createView(),
+            ]
+        );
     }
 }
--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -14,12 +14,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class HomeController extends Controller
 {
 
-
     public function indexAction()
     {
         return $this->render('index.html.twig');
     }
-
 
     public function cguAction()
     {
